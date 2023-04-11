b11311010932467816c06cb10a195e7c39b49275
volodymyrbudniak <volodymyr.budnik@300devs.com>
2023-04-11 09:20:06 +0200

    pipe fix

--- src/app/pipes/truncate.pipe.ts
+++ src/app/pipes/truncate.pipe.ts
@@ -7,7 +7,7 @@ export class TruncatePipe implements PipeTransform {
     transform(text: string , length: number = 35, suffix: string = ' ...'): string {
 
         if (text.length > length) {
-            return text.substring(0, length).trim() + suffix;
+            return text.substring(0, length) + suffix;
         }
         return text;
     }
